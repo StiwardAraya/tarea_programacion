f1ba9ef05507f9493df5768ac263c4b13f9c6f84
Stiward Araya <StiwardArayaCalderon98@gmail.com>
2024-03-04 23:10:05 -0600

    Código organizado, esta clase podria ser descartable

--- src/main/java/cr/ac/una/tarea/tarea_modelo/Buzon.java
+++ src/main/java/cr/ac/una/tarea/tarea_modelo/Buzon.java
@@ -1,14 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package cr.ac.una.tarea.tarea_modelo;
 
 /**
- *
- * @author justi
+ * Clase objeto de buzón
+ * @author Justin Mendez
+ * @author Stiward Araya
  */
 public class Buzon {
     public Buzon() {}
-     public void depositarSobreDinero(){}
+    
+    public void depositarSobreDinero(){
+        //TODO
+    }
 }
